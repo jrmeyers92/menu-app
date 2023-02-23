e3b7f130c6d6206a105df7953d0f4f0901b6714a
jrmeyers92 <JakeRMeyers92@gmail.com>
2023-02-22 20:12:25 -0600

    messing around with dummy menu data

---
+++ menuData.js
@@ -0,0 +1,67 @@
+export const categories = [
+  "appetizers",
+  "salads",
+  "burgers and sandwhiches",
+  "entrees",
+  "sides",
+  "drinks",
+  "deserts",
+];
+
+export const menuItems = [
+  {
+    name: "CHEESY BACON TRUFFLE FRIES",
+    price: 8.99,
+    calories: 1170,
+    description:
+      "Steak Fries topped with five cheese sauce, shredded Parmesan, diced tomatoes, bacon crumbles and a drizzle of mushroom & truffle aioli.",
+    categories: ["appetizers", "salads"],
+  },
+  {
+    name: "MOZZARELLA STICKS",
+    price: 9.99,
+    calories: 1250,
+    description:
+      "Lightly battered Mozzarella cheese, fried to perfection and served with marinara sauce.",
+    categories: ["appetizers"],
+  },
+  {
+    name: "TOWERING ONION RINGS",
+    price: 11.49,
+    calories: 1290,
+    description: "13 crispy onion rings with Campfire Mayo and ranch.",
+    categories: ["appetizers"],
+  },
+  {
+    name: "HOUSE SALAD",
+    price: 6.99,
+    calories: 80,
+    description:
+      "Diced tomato, sliced cucumber, shredded Cheddar cheese and croutons on mixed greens with choice of dressing.",
+    categories: ["salads"],
+  },
+  {
+    name: "AVO-COBB-O",
+    price: 14.49,
+    calories: 500,
+    description:
+      "Grilled chicken breast, hardwood-smoked bacon, Bleu cheese crumbles, hard-boiled eggs, tomatoes, croutons and avocado on mixed greens. Served with choice of dressing.",
+    categories: ["salads"],
+  },
+  {
+    name: "STEAK FRIES",
+    price: 2.99,
+    calories: 360,
+    description:
+      "Thick cut and fried to perfection with Red's Original seasoning.",
+    categories: ["sides"],
+  },
+  {
+    name: "TERIYAKI CHICKEN",
+    price: 14.99,
+    calories: 780,
+    description:
+      "Teriyaki, grilled pineapple, Swiss, lettuce, tomatoes and mayo.",
+    categories: ["Burgers and Sandwhiches"],
+  },
+];
